c68bad8d11475bd4f46a73dcaed7fb52e4c506da
Vignesh <vignesh@Vigneshs-MacBook-Air.local>
2022-06-01 11:07:11 +0530

    fix: tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
   content: ['./src/index.html', './src/**/*.{html,css,js,jsx,ts,tsx}'],
   // mode: 'jit',
   // purge: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx}'],
+  darkMode:'class',
   theme: {
     extend: {}
   },
